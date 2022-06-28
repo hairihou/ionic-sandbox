bca4dd14219bdd4c2a04a3a2ed84a87861bbcb92
hairihou <38448411+hairihou@users.noreply.github.com>
2022-06-29 02:46:50 +0900

    chore

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { Subscription } from 'rxjs';
 
-import { IonContent, ViewWillEnter, ViewWillLeave } from '@ionic/angular';
+import { DatetimeCustomEvent, IonContent, ViewWillEnter, ViewWillLeave } from '@ionic/angular';
+
+import { format, parseISO } from 'date-fns';
 
 import { TabsService } from '../tabs/services/tabs.service';
 
@@ -11,13 +13,20 @@ import { TabsService } from '../tabs/services/tabs.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements ViewWillEnter, ViewWillLeave {
+export class Tab1Page implements OnInit, ViewWillEnter, ViewWillLeave {
   @ViewChild(IonContent) content: IonContent;
 
+  date = format(new Date(), 'yyyy-MM-dd');
+  minDate = format(new Date(0), 'yyyy-MM-dd');
+  maxDate = format(new Date(), 'yyyy-MM-dd');
   readonly title = 'Tab1';
   tab$ = new Subscription();
 
   constructor(private tabsService: TabsService) {}
+
+  ngOnInit(): void {
+    console.log(Date.now());
+  }
 
   ionViewWillEnter(): void {
     this.tab$ = this.tabsService.tabChange$.subscribe({
@@ -31,5 +40,13 @@ export class Tab1Page implements ViewWillEnter, ViewWillLeave {
 
   ionViewWillLeave(): void {
     this.tab$.unsubscribe();
+  }
+
+  changeDate(event: DatetimeCustomEvent): void {
+    console.log(event.detail.value);
+    console.log(parseISO(event.detail.value));
+    console.log(parseISO(event.detail.value).toISOString());
+    console.log(new Date(event.detail.value));
+    console.log(new Date(event.detail.value).toISOString());
   }
 }
